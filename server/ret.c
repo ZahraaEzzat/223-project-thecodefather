90d1a53a676ee6592e596a5714e9b50e48963224
ZahraaEzzat <s2201131107@sci.ku.edu.kw>
2023-05-14 10:58:15 +0300

    lessa

--- server/ret.c
+++ server/ret.c
@@ -10,6 +10,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
 void ret(int clientfd, char* filename) {
 puts("I AM RETRIEVE");
@@ -17,41 +18,78 @@ puts("I AM RETRIEVE");
 	struct dirent *ent;
 	struct stat file_stat;
 
-	dir = opendir(".");
-	if (dir == NULL) {
-		printf("Failed to open directory\n");
-		return;
-	}
+	if (strcmp(filename, "*") == 0) { // retrieve all files
+		dir = opendir(".");
+		if (dir == NULL) {
+			printf("Failed to open directory\n");
+			return;
+		}
 
-	//loop through all files in directory
-	while ((ent = readdir(dir)) != NULL) {
-		if (fnmatch(filename, ent->d_name, FNM_NOESCAPE) == 0) {
-			//get file stats
-			if (stat(ent->d_name, &file_stat) == -1) {
-				printf("Failed to get file stats\n");
-				return;
-			}
-
-			//send file size to client
-			if (send(clientfd, &file_stat.st_size, sizeof(file_stat.st_size), 0) == -1) {
-				printf("Failed to send file size\n");
-				return;
-			}
-			//send file content to client
-			int file_fd = open(ent->d_name, O_RDONLY);
-			char buffer[1024];
-			ssize_t read_size;
-			while ((read_size = read(file_fd, buffer, sizeof(buffer))) > 0) {
-				if (send(clientfd, buffer, read_size, 0) == -1) {
-					printf("Failed to send file content\n");
+		// loop through all files in directory
+		while ((ent = readdir(dir)) != NULL) {
+			if (ent->d_type == DT_REG) { // only regular files
+				// get file stats
+				if (stat(ent->d_name, &file_stat) == -1) {
+					printf("Failed to get file stats\n");
 					return;
 				}
-			}
-			close(file_fd);
-		}
-	}
-	send(clientfd, "0", sizeof("0"), 0);
-	send(clientfd, "No file to retrieve", sizeof("No file to retrieve"), 0);
 
-	closedir(dir);
+				// send file size to client
+				if (send(clientfd, &file_stat.st_size, sizeof(file_stat.st_size), 0) == -1) {
+					printf("Failed to send file size\n");
+					return;
+				}
+
+				// send file content to client
+				int file_fd = open(ent->d_name, O_RDONLY);
+				char buffer[1024];
+				ssize_t read_size;
+				while ((read_size = read(file_fd, buffer, sizeof(buffer))) > 0) {
+					if (send(clientfd, buffer, read_size, 0) == -1) {
+						printf("Failed to send file content\n");
+						return;
+					}
+				}
+				close(file_fd);
+			}
+		}
+		closedir(dir);
+	} else { // retrieve a single file
+		dir = opendir(".");
+		if (dir == NULL) {
+			printf("Failed to open directory\n");
+			return;
+		}
+
+		//loop through all files in directory
+		while ((ent = readdir(dir)) != NULL) {
+			if (fnmatch(filename, ent->d_name, FNM_NOESCAPE) == 0) {
+				//get file stats
+				if (stat(ent->d_name, &file_stat) == -1) {
+					printf("Failed to get file stats\n");
+					return;
+				}
+
+				//send file size to client
+				if (send(clientfd, &file_stat.st_size, sizeof(file_stat.st_size), 0) == -1) {
+					printf("Failed to send file size\n");
+					return;
+				}
+				//send file content to client
+				int file_fd = open(ent->d_name, O_RDONLY);
+				char buffer[1024];
+				ssize_t read_size;
+				while ((read_size = read(file_fd, buffer, sizeof(buffer))) > 0) {
+					if (send(clientfd, buffer, read_size, 0) == -1) {
+						printf("Failed to send file content\n");
+						return;
+					}
+				}
+				close(file_fd);
+			}
+		}
+		return;
+	}
+	send(clientfd, "No file to retrieve", sizeof("No file to retrieve"), 0);
+	send(clientfd, "No file to retrieve", sizeof("No file to retrieve"), 0);
 }
